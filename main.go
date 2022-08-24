120a4bb21d413d382c395d1528873e27eabc9610
Abanoub Fathy <abanoub.fathy.me@gmail.com>
2022-08-25 00:03:13 +0200

    add first view template

--- main.go
+++ main.go
@@ -2,15 +2,26 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
+// home template
+var homeTemplate *template.Template
+
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/", Hello)
+	r.HandleFunc("/", Home)
 	r.NotFoundHandler = http.HandlerFunc(NotFound)
+
+	// parse the home template
+	if t, err := template.ParseFiles("views/home.gohtml"); err != nil {
+		panic(err)
+	} else {
+		homeTemplate = t
+	}
 
 	// start the app
 	fmt.Println("🚀🚀 Server is working on http://localhost:3000")
@@ -19,10 +30,10 @@ func main() {
 	}
 }
 
-// Hello is the handlerFunc for the home page
-func Hello(w http.ResponseWriter, r *http.Request) {
+// Home is the handlerFunc for the home page
+func Home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Hello in bebo gallery</h1>")
+	homeTemplate.Execute(w, nil)
 }
 
 // NotFound is the handlerFunc for not found page
