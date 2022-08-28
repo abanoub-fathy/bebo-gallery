82135901116c969123d744ef6193a06eafb7aaef
Abanoub Fathy <abanoub.fathy.me@gmail.com>
2022-08-28 14:00:30 +0200

    parse template with all layouts inside layouts Dir

--- views/views.go
+++ views/views.go
@@ -2,6 +2,7 @@ package views
 
 import (
 	"html/template"
+	"path/filepath"
 )
 
 type View struct {
@@ -11,8 +12,14 @@ type View struct {
 // NewView is a constructor function used to create new view
 // executable template parsed with layouts
 func NewView(files ...string) *View {
+	// get all files in the layout directory
+	layoutFiles, err := filepath.Glob("./views/layouts/*.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
 	// apeend fileName wihh layout files
-	files = append(files, "views/layouts/footer.gohtml")
+	files = append(files, layoutFiles...)
 
 	// parse template file with layout files
 	t, err := template.ParseFiles(files...)
