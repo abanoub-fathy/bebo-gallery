113cf0bb576261bd3e5b7d26a55511a3ea5aa0b7
Abanoub Fathy <abanoub.fathy.me@gmail.com>
2023-02-08 14:58:17 +0200

    start implementing gallery service

--- model/gallery.go
+++ model/gallery.go
@@ -1,10 +1,37 @@
 package model
 
-import uuid "github.com/satori/go.uuid"
+import (
+	uuid "github.com/satori/go.uuid"
+	"gorm.io/gorm"
+)
 
 // Gallery is the container for images we will add
 type Gallery struct {
 	Base
 	Title  string    `gorm:"not_null"`
 	UserID uuid.UUID `gorm:"not_null;index"`
+}
+
+type GalleryService interface {
+	GalleryDB
+}
+
+// GalleryDB has all methods needed to implemnt and
+// use the Gallery database methods
+type GalleryDB interface {
+	// CreateGallery is used to create a new gallery into the DB
+	CreateGallery(gallery *Gallery) error
+}
+
+// galleryGorm is the type that will implements the
+// the GalleryDB for gorm
+type galleryGorm struct {
+	db *gorm.DB
+}
+
+// making sure that galleryGorm implemnts the GalleryDB
+var _ GalleryDB = (*galleryGorm)(nil)
+
+func (gg *galleryGorm) CreateGallery(gallery *Gallery) error {
+	return gg.db.Create(&gallery).Error
 }
